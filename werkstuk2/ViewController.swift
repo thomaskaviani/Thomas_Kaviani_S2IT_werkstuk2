aa68eac7311c7e5e942017c2ade39ee5e7038ece
KAVIANI Thomas (s) <thomas.kaviani@DT-VM-OSX-25.ehb.local>
2018-06-01 14:43:55 +0200

    Aanmaak projectstructuur

---
+++ werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  werkstuk2
+//
+//  Created by KAVIANI Thomas (s) on 01/06/2018.
+//  Copyright © 2018 KAVIANI Thomas (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
